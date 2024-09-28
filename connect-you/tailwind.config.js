f9e03f09e923fb17843ca1a23be38d5644eada05
sababashfak <sababashfak.fahim1999@gmail.com>
2024-09-28 18:30:49 +0600

    Initial commit

---
+++ connect-you/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "green" : "#39DB4A",
+        "red" : "#FF6868"
+      }
+
+
+    },
+  },
+  plugins: [],
+}
+
