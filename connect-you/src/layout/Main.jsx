7f7107d9311999a088e330d9ca4ee23efa78a8e8
sababashfak <sababashfak.fahim1999@gmail.com>
2024-09-28 21:20:40 +0600

    NavBar add from daisyUI and Upload Logo

--- connect-you/src/layout/Main.jsx
+++ connect-you/src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import NavBar from '../components/NavBar'
 
 const Main = () => {
   return (
     <div>
-        <nav>Nav Bar</nav>
+        <NavBar/>
         <Outlet/>
         <footer>Footer</footer>
     </div>
