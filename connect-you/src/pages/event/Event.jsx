73c7943040ae42b520c8e184a15a9bcd54d9b3fd
sababashfak <sababashfak.fahim1999@gmail.com>
2024-10-06 02:59:38 +0600

    Complete the pagination system in Event Page Last Part

--- connect-you/src/pages/event/Event.jsx
+++ connect-you/src/pages/event/Event.jsx
@@ -1,92 +1,98 @@
 import React, { useEffect, useState } from "react";
 import Cards from "../../components/Cards";
-import { FaFilter } from "react-icons/fa"
+import { FaFilter } from "react-icons/fa";
 
 const Event = () => {
-    const[event, setEvent] =useState([]);
-    const[filteredEvents, setFilteredEvents] =useState([]);
-    const[selectedCategory, setSelectedCategory] =useState("all");
-    const[sortOption, setSortOption] =useState("default");
+  const [event, setEvent] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [eventsPerPage] = useState(9);
 
-
-    //loading data
-    useEffect(() => {
-        //fetch data from the backened
-        const fetchData = async () => {
-            try {
-                const response = await fetch('/events.json');
-                const data = await response.json();
-                //console.log(data)
-                setEvent(data);
-                setFilteredEvents(data);
-
-            } catch(error){
-                console.log("Error fetching data", error);
-            }
-        };
-
-        //call the function
-        fetchData();
-    }, [])
-
-
-
-
-    //Filteering data based on category
-    const filterEvents = (category) => {
-        const filtered =category === "all" 
-          ? event 
-          : event.filter((ev1) => ev1.category === category);
-
-      setFilteredEvents(filtered);
-      setSelectedCategory(category);
-
+  //loading data
+  useEffect(() => {
+    //fetch data from the backened
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/events.json");
+        const data = await response.json();
+        //console.log(data)
+        setEvent(data);
+        setFilteredEvents(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
     };
 
+    //call the function
+    fetchData();
+  }, []);
 
-// Show all data
+  //Filteering data based on category
+  const filterEvents = (category) => {
+    const filtered =
+      category === "all"
+        ? event
+        : event.filter((ev1) => ev1.category === category);
+
+    setFilteredEvents(filtered);
+    setSelectedCategory(category);
+
+    setCurrentPage(1);
+  };
+
+  // Show all data
   const showAll = () => {
     setFilteredEvents(event);
     setSelectedCategory("all");
 
-  }
+    setCurrentPage(1);
+  };
 
   // Sorting based on A-Z, Z-A Low - High pricing
 
-  const handleSortChange= (option) => {
+  const handleSortChange = (option) => {
     setSortOption(option);
 
     let sortedEvents = [...filteredEvents];
 
     // logic
-    switch(option){
+    switch (option) {
       case "A-Z":
-        sortedEvents.sort((a,b) => a.title.localeCompare(b.title))
+        sortedEvents.sort((a, b) => a.title.localeCompare(b.title));
         break;
       case "Z-A":
-        sortedEvents.sort((a,b) => b.title.localeCompare(a.title))
+        sortedEvents.sort((a, b) => b.title.localeCompare(a.title));
         break;
       case "low-high":
-        sortedEvents.sort((a,b) => a.price-b.price)
+        sortedEvents.sort((a, b) => a.price - b.price);
         break;
-      case "hight-low":
-        sortedEvents.sort((a,b) => b.price-a.price)
+      case "high-low":
+        sortedEvents.sort((a, b) => b.price - a.price);
         break;
       case "default": // Added sorting for default
-      sortedEvents.sort((a, b) => {
+        sortedEvents.sort((a, b) => {
           if (a.featured === b.featured) {
-              return 0; // No change in order
+            return 0; // No change in order
           }
           return a.featured ? -1 : 1; // featured events come first
-      });
-      break;
+        });
+        break;
       default:
         break;
     }
 
     setFilteredEvents(sortedEvents);
-  }
+    setCurrentPage(1);
+  };
 
+  // pagination logic
+
+   const indexOfLastEvent = currentPage * eventsPerPage;
+   const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
+   const currentEvents = filteredEvents.slice(indexOfFirstEvent, indexOfLastEvent);
+   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
@@ -116,90 +122,103 @@ const Event = () => {
             </div>
           </div>
         </div>
-
-        {/* Event Ticket Section */}
-        <div className="section-container">
-          {/* filtering & sorting */}
-          <div className="flex flex-col md:flex-row flex-warp md:justify-between items-center space-y-3 mb-8">
-            {/* all category btns */}
-            <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
-              <button
-                onClick={showAll}
-                className={selectedCategory === "all" ? "active" : ""}
-              >
-                All
-              </button>
-              <button
-                onClick={() => filterEvents("Reunion")}
-                className={selectedCategory === "Reunion" ? "active" : ""}
-              >
-                Reunion
-              </button>
-              <button
-                onClick={() => filterEvents("Music")}
-                className={selectedCategory === "Music" ? "active" : ""}
-              >
-                Music
-              </button>
-              <button
-                onClick={() => filterEvents("Skill Development")}
-                className={
-                  selectedCategory === "Skill Development" ? "active" : ""
-                }
-              >
-                Skill Development
-              </button>
-              <button
-                onClick={() => filterEvents("Expo")}
-                className={selectedCategory === "Expo" ? "active" : ""}
-              >
-                Expo
-              </button>
-              <button
-                onClick={() => filterEvents("Seminar")}
-                className={selectedCategory === "Seminar" ? "active" : ""}
-              >
-                Seminar
-              </button>
-              <button
-                onClick={() => filterEvents("Fest")}
-                className={selectedCategory === "Fest" ? "active" : ""}
-              >
-                Fest
-              </button>
-            </div>
-            {/* sorting based filtering */}
-            <div className="flex justify-end mb-4 rounded-sm">
-              <div className="bg-orange p-2">
-                <FaFilter className="h-4 w-4 text-white " />
-              </div>
-
-              {/* sorting options */}
-              <select
-                name="sort"
-                idd="sort"
-                onChange={(e) => handleSortChange(e.target.value)}
-                value={sortOption}
-                className="bg-white text-black border-black border-2 px-2 py-1 rounded-sm"
-              >
-                <option value="default">Default</option>
-                <option value="A-Z">A - Z</option>
-                <option value="Z-A">Z - A</option>
-                <option value="low-high">Low - High</option>
-                <option value="high-low">High - Low</option>
-              </select>
-            </div>
+      </div>
+      {/* Event Ticket Section */}
+      <div className="section-container">
+        {/* filtering & sorting */}
+        <div className="flex flex-col md:flex-row flex-warp md:justify-between items-center space-y-3 mb-8">
+          {/* all category btns */}
+          <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
+            <button
+              onClick={showAll}
+              className={selectedCategory === "all" ? "active" : ""}
+            >
+              All
+            </button>
+            <button
+              onClick={() => filterEvents("Reunion")}
+              className={selectedCategory === "Reunion" ? "active" : ""}
+            >
+              Reunion
+            </button>
+            <button
+              onClick={() => filterEvents("Music")}
+              className={selectedCategory === "Music" ? "active" : ""}
+            >
+              Music
+            </button>
+            <button
+              onClick={() => filterEvents("Skill Development")}
+              className={
+                selectedCategory === "Skill Development" ? "active" : ""
+              }
+            >
+              Skill Development
+            </button>
+            <button
+              onClick={() => filterEvents("Expo")}
+              className={selectedCategory === "Expo" ? "active" : ""}
+            >
+              Expo
+            </button>
+            <button
+              onClick={() => filterEvents("Seminar")}
+              className={selectedCategory === "Seminar" ? "active" : ""}
+            >
+              Seminar
+            </button>
+            <button
+              onClick={() => filterEvents("Fest")}
+              className={selectedCategory === "Fest" ? "active" : ""}
+            >
+              Fest
+            </button>
           </div>
-          {/* <div className="flex h-3">
+          {/* sorting based filtering */}
+          <div className="flex justify-end mb-4 rounded-sm">
+            <div className="bg-orange p-2">
+              <FaFilter className="h-4 w-4 text-white " />
+            </div>
 
-          </div> */}
-          {/* Event Cards */}
-          <div className="grid  lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 ">
-            {filteredEvents.map((ev) => (
-              <Cards key={ev._id} ev={ev} />
-            ))}
+            {/* sorting options */}
+            <select
+              name="sort"
+              id="sort"
+              onChange={(e) => handleSortChange(e.target.value)}
+              value={sortOption}
+              className="bg-white text-black border-black border-2 px-2 py-1 rounded-sm"
+            >
+              <option value="default">Default</option>
+              <option value="A-Z">A - Z</option>
+              <option value="Z-A">Z - A</option>
+              <option value="low-high">Low - High</option>
+              <option value="high-low">High - Low</option>
+            </select>
           </div>
         </div>
+        {/* <div className="flex h-3">
+
+          </div> */}
+        {/* Event Cards */}
+        <div className="grid  lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 ">
+          {currentEvents.map((ev) => (
+            <Cards key={ev._id} ev={ev} />
+          ))}
+        </div>
+      </div>
+
+      {/* pagination section */}
+      <div className="flex justify-center my-8">
+       {
+         Array.from({length: Math.ceil(filteredEvents.length / eventsPerPage)}).map((_, index) => (
+          <button key={index +1} 
+          onClick={() => paginate(index+1) }
+          className={`mx-1 px-3 py-1 rounded-full ${currentPage === index+1 ? "bg-orange text-white" : "bg-gray-200" }`}
+          >
+            {index + 1}
+          </button>
+         ))
+       }
       </div>
     </div>
   );
